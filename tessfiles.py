3f8e34392fe329cd2378e10d1dc55b80ab5f0321
David Cormier <dcormier@barbarie.localdomain>
2011-11-05 12:10:04 -0400

    Enlevé ocropus, ajout classe Page et ligne

--- tessfiles.py
+++ tessfiles.py
@@ -8,31 +8,35 @@ import logging
 import subprocess, shlex
 from datetime import datetime
 
-def prepare_directory(directory):
-    """Convertit les images d'un répertoire en tif et fait l'ocr dessus"""
-    print "Prépare le répertoire %s" % directory
-     #1 Appeler mogrify pour convertir les png en tif
-    cmd_mogrify = "mogrify -format tif %s/*.png" % directory
-    args_mogrify = shlex.split(cmd_mogrify)
-    p = subprocess.call(args_mogrify)
+class Ligne(object):
+    """Ligne d'une page"""
+    def __init__(self, chars):
+        self.chars = chars
 
-    walker = os.walk(directory)
-    for w in walker:
-        root, dirs, files = w
-        for _dir in dirs:
-            #Traite récursivement les sous-répertoires
-            prepare_directory("%s/%s" % (directory, _dir))
-        for fichier in files:
-            filename, file_ext = os.path.splitext(fichier)
-            if file_ext == '.tif':
-                tess_line = '/usr/bin/tesseract "%s/%s" "%s/%s" -l fra' % (directory, fichier, directory, filename)
-                tess_box = "%s makebox" % tess_line
-                print tess_line
-                args = shlex.split(tess_line)
-                args2 = shlex.split(tess_box)
-                p = subprocess.call(args, stdout=subprocess.PIPE)
-                p = subprocess.call(args2, stdout=subprocess.PIPE)
-             
+    def __str__(self):
+        return str(self.chars)
+
+class Page(object):
+    """Page d'un livre numérisé"""
+    def __init__(self, txt, box):
+        """Initialise une page à partir de son boxfile et de son
+           fichier tesseract"""
+        self.txt = txt
+        self.lignes = []
+        boxes = [{'char':b[0],
+                    'x1':b[1],
+                    'y1':b[2],
+                    'x2':b[3],
+                    'y2':b[4]}
+                  for b in open(box)]
+        offset = 0
+        with open(self.txt) as f:
+            for ligne in f:
+                length = len(ligne.replace(' ', ''))
+                self.lignes.append(Ligne(boxes[offset:length]))
+                offset += length 
+                
+
 if __name__ == '__main__':
     #Configure le logger
     now = datetime.now()
@@ -47,7 +51,14 @@ if __name__ == '__main__':
   
     ## Fichiers initialement présents dans la racine.
     fichiers_racine = [f for f in os.listdir(parametres.racine)]
- 
+
+    tess_line = '/usr/bin/tesseract "%s/%s.tif" "%s/%s" -l fra'
+    tess_box = "%s makebox" % tess_line
+
+    commandes = [(tess_line % (parametres.racine, os.path.splitext(f)[0], parametres.racine, os.path.splitext(f)[0]),
+                  tess_box % (parametres.racine, os.path.splitext(f)[0], parametres.racine, os.path.splitext(f)[0]))
+                 for f in os.listdir(parametres.racine)]
+
     #1 Appeler mogrify pour convertir les png en tif
     if parametres.conversion_tif:
         cmd_mogrify = "mogrify -format tif %s/*.png" % parametres.racine
@@ -55,25 +66,7 @@ if __name__ == '__main__':
         args_mogrify = shlex.split(cmd_mogrify)
         print args_mogrify
         p = subprocess.call(args_mogrify)
-
     
-    #2 Appeler ocropus p-seg. pour faire les zones
-    for f in fichiers_racine:
-        filename, file_ext = os.path.splitext(f) 
-        cmd_opseg = "ocropus-pseg %s/%s%s" % (parametres.racine, filename, ".tif")
-        print cmd_opseg
-        args_opseg = shlex.split(cmd_opseg)
-        p = subprocess.call(args_opseg, stdout=subprocess.PIPE)
-
-
-    #3 pour chacun des répertoires
-    prepare_directory(parametres.racine)
-    #faire tesseract
-    for f in fichiers_racine:
-        filename, file_ext = os.path.splitext(f)
-        cmd_hocr = "ocropus-hocr %s > %s" % (f, "%s.html" % filename)
-        args_hocr = shlex.split(cmd_hocr)
-        print cmd_hocr
-        p = subprocess.call(args_hocr, stdout=subprocess.PIPE)
-        print "ocr fait!"
-
+    for tess_cmd, box_cmd in commandes:
+        subprocess.call(shlex.split(tess_cmd))
+        subprocess.call(shlex.split(box_cmd))
