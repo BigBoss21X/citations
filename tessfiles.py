9106c57f16791265c2276f021d43fc186d464803
David Cormier <dcormier@barbarie.localdomain>
2011-11-06 06:21:55 -0500

    Enregistre la page en html avec jinja2

--- tessfiles.py
+++ tessfiles.py
@@ -6,12 +6,27 @@ import sys
 import argparse
 import logging
 import subprocess, shlex
+import codecs
+
 from datetime import datetime
+
+from jinja2 import Environment, PackageLoader
+env = Environment(loader=PackageLoader('tessfiles', 'templates'))
 
 class Ligne(object):
     """Ligne d'une page"""
-    def __init__(self, chars):
+    def __init__(self,ligne, chars):
         self.chars = chars
+        #On trouve le X le plus à "gauche"
+        self.ligne = ligne
+        if chars:
+            x1 = sorted(chars, key=lambda a: a['x1'])[0]['x1']
+            x2 = sorted(chars, key=lambda a: a['x2'], reverse=True)[0]['x2']
+            y1 = sorted(chars, key=lambda a: a['y1'])[0]['y1']
+            y2 = sorted(chars, key=lambda a: a['y2'], reverse=True)[0]['y2']
+
+            self.longueur = x2-x1
+            self.hauteur = y2-y1
 
     def __str__(self):
         return str(self.chars)
@@ -22,20 +37,26 @@ class Page(object):
         """Initialise une page à partir de son boxfile et de son
            fichier tesseract"""
         self.txt = txt
+        self.filename = os.path.splitext(txt)[0]
         self.lignes = []
-        boxes = [{'char':b[0],
-                    'x1':b[1],
-                    'y1':b[2],
-                    'x2':b[3],
-                    'y2':b[4]}
+        boxes = [{'char':b.split(' ')[0],
+                    'x1':int(b.split(' ')[1]),
+                    'y1':int(b.split(' ')[2]),
+                    'x2':int(b.split(' ')[3]),
+                    'y2':int(b.split(' ')[4])}
                   for b in open(box)]
         offset = 0
         with open(self.txt) as f:
             for ligne in f:
-                length = len(ligne.replace(' ', ''))
-                self.lignes.append(Ligne(boxes[offset:length]))
-                offset += length 
-                
+                line = ligne.rstrip('\n').decode('utf-8')
+                length = len(line.replace(' ', ''))
+                self.lignes.append(Ligne(line, boxes[offset:length+offset]))
+                offset += length
+
+    def as_html(self):
+        with codecs.open("%s.html" % self.filename, 'w', encoding='utf-8') as f:
+            template = env.get_template('page.html')
+            f.write(template.render(lignes=self.lignes))
 
 if __name__ == '__main__':
     #Configure le logger
