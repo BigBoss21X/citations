adeaad78a74ad93f027264356bc265e903fd21bd
David Cormier <david.cormier@savoirfairelinux.com>
2011-12-06 16:36:58 -0500

    [#13] Parser google scholar

---
+++ bibliographie/bibliographie.py
@@ -0,0 +1,64 @@
+#-*- coding: utf-8 -*-
+import sys
+import shlex
+import StringIO
+from subprocess import Popen, PIPE
+from pybtex.database.input import bibtex
+import os
+import urllib
+from BeautifulSoup import BeautifulSoup
+
+class MyUrlOpener(urllib.FancyURLopener):
+    version = "Lynx/2.8.8dev.3 libwww-FM/2.14 SSL-MM/1.4.1"
+
+def get_bibtex(citation):
+    """Retourne l'entrée bibtex pour une citation"""
+    abspath = os.path.abspath('./lib/anystyle-wrapper.rb')
+    cmd_anystyle = ["ruby", "%s" % abspath, "%s" % citation]
+    p = Popen(cmd_anystyle, stdout=PIPE)
+    strio = StringIO.StringIO(p.communicate()[0])
+    parser = bibtex.Parser()
+    return parser.parse_stream(strio)
+
+class ChercheurRessource(object):
+    """ classe qui permet de chercher les ressources"""
+    def __init__(self, engins):
+        self.engins = engins
+        urllib._urlopener = MyUrlOpener()
+        self.fichier = 1
+    def chercher(self, entree):
+        for engin in self.engins:
+            url_requete = engin.url_recherche(entree)
+            raw_res = urllib.urlopen(url_requete).read()
+            html = BeautifulStoneSoup(raw_res)
+            with open("url-%s" % self.fichier, 'w') as f:
+                f.write(raw_res)
+            self.fichier += 1
+
+
+class EnginGoogle(object):
+    def __init__(self, url_base):
+        self.url_base = url_base
+    def url_recherche(self, entree):
+        url = self.url_base
+        if 'title' in entree.fields:
+            url = "%sintitle:%s" % (url, urllib.quote(entree.fields['title']))
+        if 'author' in entree.persons:
+            url = "%s+inauthor:%s" % (url, urllib.quote(entree.persons['author'][0].last()[0]))
+
+        return url
+
+if __name__ == '__main__':
+    biblio = bibtex.Parser().parse_file(sys.argv[1])
+    cr = ChercheurRessource([EnginGoogle('http://scholar.google.com/scholar?q=')])
+    for i in range(1,9):
+        print "======================================="
+        print "======================================="
+        with open('url-%s' % i) as f:
+            html = BeautifulSoup("".join(f.readlines()))
+            gs_rt = html.find('div', { 'class' : 'gs_rt' })
+            if gs_rt:
+                link = gs_rt.find('a')
+                if link:
+                    print link.attrs[0][1]
+
